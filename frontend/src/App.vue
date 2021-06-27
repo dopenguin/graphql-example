1f0a1831cda79d32a4e9a1612e5b5fb4d62ac5c2
Pascal Röhling <pascal.roehling@dataport.de>
2021-06-27 22:53:32 +0200

    🔥 Updated CSS and added a dependency for HOT RELOADING.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,33 +11,41 @@ export default {
 </script>
 
 <style lang="scss">
+$btn-color-bright: #d1a860;
+$btn-color-dark: #9e6809;
+
 body,
 html {
   height: 99%;
+}
+body {
+  background: linear-gradient(120deg, #cde099, #7c9e1c);
 }
 .grid {
   display: grid;
   place-items: center;
   height: 100%;
-}
-#anchor {
-  background-color: darkolivegreen;
-  border: 1px solid black;
-  box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.2);
-  color: white;
-  cursor: pointer;
-  font-size: 16px;
-  margin: 4px 2px;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  transition: all 1s ease;
-  width: 10rem;
 
-  &:hover {
-    background-color: lightgreen;
-    box-shadow: 0 6px 25px 0 rgba(0, 0, 0, 0.6);
-    color: black;
+  #anchor {
+    background: linear-gradient(to right, $btn-color-dark, $btn-color-bright);
+    background-size: 150%;
+    box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.2);
+    color: white;
+    cursor: pointer;
+    font-size: 16px;
+    margin: 4px 2px;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    transition-property: background-position, box-shadow;
+    transition-duration: 1s;
+    transition-timing-function: ease;
+    width: 10rem;
+
+    &:hover {
+      background-position: right;
+      box-shadow: 0 6px 25px 0 rgba(0, 0, 0, 0.6);
+    }
   }
 }
 </style>
